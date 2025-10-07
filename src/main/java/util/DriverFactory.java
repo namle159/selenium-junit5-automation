9714b7407c045e80a94961c1ee5975478e3a9bc9
Le Ngoc Nam <lenam1591999@gmail.com>
2025-10-07 21:19:07 +0700

    Initial commit: Selenium + JUnit5 + CI

--- src/main/java/util/DriverFactory.java
+++ src/main/java/util/DriverFactory.java
@@ -3,17 +3,42 @@ package util;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class DriverFactory {
 
     public static WebDriver createDriver(String browser) {
+        WebDriver driver;
+
         if (browser.equalsIgnoreCase("firefox")) {
             WebDriverManager.firefoxdriver().setup();
-            return new FirefoxDriver();
+
+            FirefoxOptions options = new FirefoxOptions();
+            options.addArguments("--width=1920");
+            options.addArguments("--height=1080");
+            options.addArguments("--headless"); // chạy ẩn trên CI
+            driver = new FirefoxDriver(options);
+
         } else {
             WebDriverManager.chromedriver().setup();
-            return new ChromeDriver();
+
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless=new"); // Chrome headless mode mới
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--disable-extensions");
+            options.addArguments("--remote-allow-origins=*");
+            options.addArguments("--window-size=1920,1080");
+
+            // ⚙️ Fix lỗi "user data directory is already in use"
+            options.addArguments("--user-data-dir=/tmp/chrome-profile-" + System.currentTimeMillis());
+
+            driver = new ChromeDriver(options);
         }
+
+        return driver;
     }
 }
